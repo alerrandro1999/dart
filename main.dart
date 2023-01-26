d7624bdfbbb9aa8e26932584add2c179f116e834
alerrandro borges <alerrandrokaton@gmail.com>
2023-01-26 17:55:55 -0300

    operadores de teste e de tipo (operators)

--- main.dart
+++ main.dart
@@ -1,12 +1,16 @@
-
 void main() {
-  print(2 == 2);
-  print(2 != 2);
-  print(2 < 2);
-  print(2 > 2);
-  print(2 >= 2);
-  print(2 <= 2);
+  // is
+  // is!
+  // as3
 
+  final numero = 0
+
+  print(int is Object);
+  print(double is Object);
+  print(num is Object);
+  print(String is Object);
+  print(bool is Object);
 
 }
 
+https://youtu.be/AqqRB2ATZ6I?list=PLK5FPzMuRKlyiWZUUqea2Hmszhy9vUixJ&t=147
