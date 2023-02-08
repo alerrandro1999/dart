05b9e47e81fdc34ba8d6b51feba5946fe0d2141c
alerrandro borges <alerrandrokaton@gmail.com>
2023-02-08 17:33:07 -0300

    if e else (statements)

--- main.dart
+++ main.dart
@@ -1,30 +1,12 @@
-import 'dart:html';
-
 void main() {
-  // final container = Container();
+  final idade = 11;
 
-  // container.cor = "blue";
-  // container.altura = 1.90;
-  // container.largura = 1.50;
-
-  // final container_2 = Container()
-  // ..cor = "black"
-  // ..altura = 2.20
-  // ..largura = 1.50;
-  // final Container = getContainer()
-  // ?..altura = 1.4
-  // ..largura = 1.0
-  // ..cor = 'blue';
-
-  document.querySelector('#confirmar')?..text = 'confirmar'..classes.add('blue')..onClick.listen((event) => window.alert('Ops, fala tu'));
+  if(idade >= 18){
+    print("maior de idade");
+  }else if(idade >= 13) {
+    print("adolecente");
+  }
+  else {
+    print("menor de idade");
+  }
 }
-
-// Container? getContainer() {
-//   return Container();
-// }
-
-// class Container{
-//   late String cor;
-//   late double altura;
-//   late double largura;
-// }
