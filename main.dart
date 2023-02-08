2ae1eebebbf70d7ed4504f55d77a8b70d547dc8f
alerrandro borges <alerrandrokaton@gmail.com>
2023-02-08 17:28:27 -0300

    notação em cascata (operators)

--- main.dart
+++ main.dart
@@ -1,14 +1,30 @@
+import 'dart:html';
+
 void main() {
+  // final container = Container();
 
-    // bool conteudo = true;
+  // container.cor = "blue";
+  // container.altura = 1.90;
+  // container.largura = 1.50;
 
-    // final resultado = conteudo ? 'publico' : 'privado';
+  // final container_2 = Container()
+  // ..cor = "black"
+  // ..altura = 2.20
+  // ..largura = 1.50;
+  // final Container = getContainer()
+  // ?..altura = 1.4
+  // ..largura = 1.0
+  // ..cor = 'blue';
 
-    String? conteudo = getConteudo();
-
-    final resultado = conteudo ?? 'Estuda Dart';
-
-    print(resultado);
+  document.querySelector('#confirmar')?..text = 'confirmar'..classes.add('blue')..onClick.listen((event) => window.alert('Ops, fala tu'));
 }
 
-String? getConteudo() => null;
+// Container? getContainer() {
+//   return Container();
+// }
+
+// class Container{
+//   late String cor;
+//   late double altura;
+//   late double largura;
+// }
